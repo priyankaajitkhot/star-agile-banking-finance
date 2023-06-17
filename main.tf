fd84f54e42d4991d2795b35ada46b6232ad4a30f
priyankaajitkhot <129971427+priyankaajitkhot@users.noreply.github.com>
2023-06-17 20:49:09 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAZXNLVLF6747ZWALG"
+  secret_key = "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"
+}
+
+resource "aws_instance" "my_ec2_instance" { 
+ ami = "ami-0261755bbcb8c4a84" 
+ instance_type = "t2.medium" 
+ key_name = "devops" 
+  
+ tags ={ 
+ name="Terraform hands-on" 
+} 
+   user_data = <<-EOF
+    #!/bin/bash
+    apt-get update -y
+    apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    apt-get update -y
+    apt-get install -y docker-ce docker-ce-cli containerd.io
+    usermod -aG docker ubuntu
+  EOF
+}
