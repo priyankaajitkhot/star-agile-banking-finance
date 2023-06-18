e3e871990fb1a19287f8a914d2a786479134a2da
priyankaajitkhot <129971427+priyankaajitkhot@users.noreply.github.com>
2023-06-18 10:32:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "my_ec2_instance" { 
  ami = "ami-0261755bbcb8c4a84" 
  instance_type = "t2.medium" 
- key_name = "devops" 
+ key_name = "devops.pem" 
   
  tags ={ 
  name="Terraform hands-on" 
