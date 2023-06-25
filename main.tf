ccddebd453d0bb239dd0e965bb05f370a88bf919
priyankaajitkhot <129971427+priyankaajitkhot@users.noreply.github.com>
2023-06-25 16:03:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,12 +12,11 @@ resource "aws_instance" "my_ec2_instance" {
 } 
    user_data = <<-EOF
     #!/bin/bash
-    apt-get update -y
-    apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    apt-get update -y
-    apt-get install -y docker-ce docker-ce-cli containerd.io
-    usermod -aG docker ubuntu
+       sudo apt-get update -y
+       sudo apt install docker.io -y
+       sudo systemctl enable docker
+       sudo docker run -itd -p 8090:8081 priyankaajitkhot/mycontainer:latest
+       sudo docker start $(docker ps -aq)
+    
   EOF
 }
